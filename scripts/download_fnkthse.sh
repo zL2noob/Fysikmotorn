6d4f940c55c847ac7eb7312c0f3ded19fd618cc3
Leo Trolin <leo.trolin@gmail.com>
2025-05-20 20:35:04 +0200

    Fix minor typos in download_fnkthse

--- scripts/download_fnkthse.sh
+++ scripts/download_fnkthse.sh
@@ -1,7 +1,6 @@
 source ./download.sh
 source ../.env
 
-mkdir ../services/bittan_marke_frontend 2> /dev/null
+mkdir ../services/fnkth.se 2> /dev/null
 
 install_release fysiksektionens-naringslivsnamnd/hemsida build.tar.gz ../services/fnkth.se $FNKTHSE_VERSION
-# TODO change organisation and repo name to FN's
